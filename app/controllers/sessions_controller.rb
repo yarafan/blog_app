d04775f515ec08e780be64126657c418dbcba7fe
yarafan <ubuntu@ubuntu.(none)>
2014-03-03 20:20:51 +0200

    Practise test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,8 @@ class SessionsController < ApplicationController
 	def new
   	end
   	def create
-  		user = User.find_by(email: params[:session][:email].downcase)
-    	if user && user.authenticate(params[:session][:password])
+  		user = User.find_by(email: params[:email].downcase)
+    	if user && user.authenticate(params[:password])
       sign_in user
       redirect_to user
     	else
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,3 +1,5 @@
+include ApplicationHelper
+
 def full_title(page_title)
   base_title = "Ruby on Rails Tutorial Sample App"
   if page_title.empty?
@@ -6,3 +8,17 @@ def full_title(page_title)
     "#{base_title} | #{page_title}"
   end
 end
+
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+def valid_sign(user2)
+fill_in "Name",         with: "Example User"
+fill_in "Email",        with: "user@example.com"
+fill_in "Password",     with: "foobar"
+fill_in "Confirmation", with: "foobar"
+end
+ 	
